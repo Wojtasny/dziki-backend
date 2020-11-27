f5ecfbabfa1528399bd07918084cf68e78f3472e
wojtasny <wojciech232@gmail.com>
2020-11-27 21:51:57 +0100

    slider problem

--- src/main/java/com/anotherlineofcode/reportwildboar/controller/HelloWorldController.java
+++ src/main/java/com/anotherlineofcode/reportwildboar/controller/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.anotherlineofcode.reportwildboar.controller;
 
+import com.anotherlineofcode.reportwildboar.handlereport.BoarReport;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,5 +15,17 @@ public class HelloWorldController {
     public String helloWorld(@RequestParam(required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name + " TELEMABK!");
         return "hello-world";
+    }
+
+    @GetMapping("/report")
+    public String reportForm(Model model){
+        model.addAttribute("boarReport", new BoarReport());
+        return "boar-report";
+    }
+
+    @PostMapping("/report")
+    public String reportBoar(@ModelAttribute BoarReport boarReport, Model model) {
+        model.addAttribute("boarReport", boarReport);
+        return "report-result";
     }
 }
