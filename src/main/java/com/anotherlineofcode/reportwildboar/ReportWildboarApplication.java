780847dd8d09587b62980891cf2c3b5ce8c5174b
Eryk Leniart <eryk.leniart@motorolasolutions.com>
2020-11-27 19:28:13 +0100

    Hello World spring boot init

---
+++ src/main/java/com/anotherlineofcode/reportwildboar/ReportWildboarApplication.java
@@ -0,0 +1,13 @@
+package com.anotherlineofcode.reportwildboar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportWildboarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportWildboarApplication.class, args);
+	}
+
+}
